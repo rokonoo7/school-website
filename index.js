ed637a576bad43453ab25fb26b7a4ee11e4bbfa0
rokon <modasshir007@gmail.com>
2023-09-27 14:54:06 +0600

    this is commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const btn=document.querySelector(".hamburger-icon");
-const navbar=document.querySelector(".main-header")
-let activ_func=()=>{
-    navbar.classList.toggle("active");
-}
+// const btn=document.querySelector(".hamburger-icon");
+// const navbar=document.querySelector(".main-header")
+// let activ_func=()=>{
+//     navbar.classList.toggle("active");
+// }
 
-btn.addEventListener("click",()=>activ_func());
+// btn.addEventListener("click",()=>activ_func());
+ const btn=document.querySelector(".nav-2");
+ function togglebtn(){
+    btn.classList.toggle("nav-click");
+ }
